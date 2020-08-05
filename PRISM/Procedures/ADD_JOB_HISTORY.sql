797a08cf1ea53bda10a1f1d815e7c5c07dcffb7d
Anton Marquez <anton.marquez@essilor.com>
2020-08-06 01:05:36 +0800

    Update ADD_JOB_HISTORY.sql

--- PRISM/Procedures/ADD_JOB_HISTORY.sql
+++ PRISM/Procedures/ADD_JOB_HISTORY.sql
@@ -10,7 +10,7 @@ BEGIN
   INSERT INTO job_history (employee_id, start_date, end_date,
                            job_id, department_id)
     VALUES(p_emp_id, p_start_date, p_end_date, p_job_id, p_department_id);
-   --antonm US1001 
+   --antonm US1001 test
   INSERT INTO country (country_id, country_name, region_id,
                            remarks)
     VALUES(1, 'Philippines', 1, 'Test');
